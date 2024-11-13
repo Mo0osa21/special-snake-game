2019e4eac82ebb2b314f3b08ad6277586ef3ccdc
Moosa <moosajamel.21@gmail.com>
2024-11-13 16:32:28 +0300

    snake always moves done

--- app.js
+++ app.js
@@ -152,6 +152,10 @@ let message = ''
 let level = 0
 let Speed = 0.3
 
+const messageElm = document.querySelector('#message')
+const scoreElm = document.querySelector('#score')
+const levelElm = document.querySelector('#level')
+
 const getNormalApple = () => {
   const randomIndex = Math.floor(Math.random() * 100)
   if (
@@ -178,10 +182,6 @@ const getBadApple = () => {
   }
 }
 
-const messageElm = document.querySelector('#message')
-const scoreElm = document.querySelector('#score')
-const levelElm = document.querySelector('#level')
-
 const updateBoard = () => {
   let square = ''
   board.forEach((sqr, index) => {
@@ -206,6 +206,7 @@ snakeBody.forEach((part, index) => {
 getBadApple()
 getNormalApple()
 updateBoard()
+
 document.addEventListener('keyup', (event) => {
   let pastLocation = ''
   if (event.key === 'ArrowDown' && direction !== 'up') {
@@ -278,3 +279,74 @@ document.addEventListener('keyup', (event) => {
     updateBoard()
   }
 })
+
+const moveSnake = () => {
+  if (direction === 'right') {
+    direction = 'right'
+    snakeBody.forEach((part, index) => {
+      if (index === 0) {
+        pastLocation = part
+        board[snakeBody[index].substring(6)] = ''
+        snakeBody[index] = `square${Number(part.substring(6)) + 1}`
+        board[snakeBody[index].substring(6)] = 'snake'
+      } else {
+        board[snakeBody[index].substring(6)] = ''
+        snakeBody[index] = pastLocation
+        board[snakeBody[index].substring(6)] = 'snake'
+        pastLocation = part
+      }
+    })
+    board[pastLocation.substring(6)] = ''
+    updateBoard()
+  } else if (direction === 'down') {
+    snakeBody.forEach((part, index) => {
+      if (index === 0) {
+        pastLocation = part
+        board[snakeBody[index].substring(6)] = ''
+        snakeBody[index] = `square${Number(part.substring(6)) + 12}`
+        board[snakeBody[index].substring(6)] = 'snake'
+      } else {
+        board[snakeBody[index].substring(6)] = ''
+        snakeBody[index] = pastLocation
+        board[snakeBody[index].substring(6)] = 'snake'
+        pastLocation = part
+      }
+    })
+    board[pastLocation.substring(6)] = ''
+    updateBoard()
+  } else if (direction === 'up') {
+    snakeBody.forEach((part, index) => {
+      if (index === 0) {
+        pastLocation = part
+        board[snakeBody[index].substring(6)] = ''
+        snakeBody[index] = `square${Number(part.substring(6)) - 12}`
+        board[snakeBody[index].substring(6)] = 'snake'
+      } else {
+        board[snakeBody[index].substring(6)] = ''
+        snakeBody[index] = pastLocation
+        board[snakeBody[index].substring(6)] = 'snake'
+        pastLocation = part
+      }
+    })
+    board[pastLocation.substring(6)] = ''
+    updateBoard()
+  } else if (direction === 'left') {
+    snakeBody.forEach((part, index) => {
+      if (index === 0) {
+        pastLocation = part
+        board[snakeBody[index].substring(6)] = ''
+        snakeBody[index] = `square${Number(part.substring(6)) - 1}`
+        board[snakeBody[index].substring(6)] = 'snake'
+      } else {
+        board[snakeBody[index].substring(6)] = ''
+        snakeBody[index] = pastLocation
+        board[snakeBody[index].substring(6)] = 'snake'
+        pastLocation = part
+      }
+    })
+    board[pastLocation.substring(6)] = ''
+    updateBoard()
+  }
+}
+
+setInterval(moveSnake, 200)
